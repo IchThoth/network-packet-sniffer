8dedcd525a3d2134d3b09e9905b11ed269acfacd
ichthoth <shalomtata334@gmail.com>
2024-01-26 22:58:42 +0100

    TCP and UDP

--- sniff.py
+++ sniff.py
@@ -3,7 +3,15 @@ import socket
 import textwrap
 
 
+TAB_1 = '\t - '
+TAB_2 = '\t\t - '
+TAB_3 = '\t\t\t - '
+TAB_4 = '\t\t\t\t - '
 
+DATA_TAB_1 = '\t '
+DATA_TAB_2 = '\t\t '
+DATA_TAB_3 = '\t\t\t '
+DATA_TAB_4 = '\t\t\t\t '
 
 def main(): 
     conn = socket.socket(socket.AF_PACKET,socket.SOCK_RAW, socket.ntohs(3))
@@ -11,7 +19,41 @@ def main():
         raw_data, addr = conn.recvfrom(65536)
         dest_mac, sender_mac, ethernet_protocol, data = ethernet_wrap(raw_data)
         print('\nEthernet Frame:')
-        print('Destination: {}, Source:{}, Protocol:{}',format( dest_mac, sender_mac, ethernet_protocol))
+        print(TAB_1 +'Destination: {}, Source:{}, Protocol:{}',format( dest_mac, sender_mac, ethernet_protocol))
+
+        # 8 means IPv4
+        if ethernet_protocol == 8:
+            (version, header_length, ttl, protocol, source, target, data) = ipv4_packet_unpack(data)
+            print(TAB_1 + 'IPV4 PACKET:')
+            print(TAB_2 + 'Version: {}, Header Length: {}, TTl: {}',format(version, header_length, ttl))
+            print(TAB_2 + 'Protocol:{}, Source: {}, Target: {}', format(protocol, source, target))
+            
+            # 1 is code for ICMP
+            if protocol == 1:
+                icmp_type, code, checksum, data = icmp_packet_unpack(data)
+                print(TAB_1 + 'ICMP PACKET:')
+                print(TAB_2 + 'Type: {}, Code: {}, Checksum: {}',format(icmp_type, code, checksum))
+                print(TAB_2 + 'Data: ')
+                print(format_lines(DATA_TAB_3,data))
+            # 6 s code for TCP
+            elif protocol == 6:
+                (source_port, destination_port,sequence, acknowledgement, flag_urg, flag_ack, flag_psh, flag_rst, flag_syn, flag_fin, data) = tcp_segment_unpack(data)
+                print(TAB_1 + 'TCP Segment:')
+                print(TAB_2 + 'Source Port: {}, Destination Port: {}', format(source_port,destination_port))
+                print(TAB_2 + 'Sequence: {},  Acknowledgement: {}', format(sequence,acknowledgement))
+                print(TAB_2 + 'Flags:')
+                print(TAB_3 + 'URG: {}, ACK: {}, PSH:{}, RST:{}, SYN:{}, FIN:{},', format(flag_urg, flag_ack, flag_psh, flag_rst, flag_syn, flag_fin))
+                print(TAB_2 + 'Data: ')
+                print(format_lines(DATA_TAB_3,data))
+
+            # 7 is code for UDP 
+            elif protocol == 7:
+                source_port, destination_port,length, data = udp_segment_unpack(data)
+                print(TAB_1 + 'UDP Segment:')
+                print(TAB_2 + 'Source Port: {}, Destination Port: {}, Length: {}', format(source_port,destination_port,length))
+
+            # support for other internet protocols comes later
+
 
 #get ethernet frame
 #recever 6 bytes -->
@@ -58,7 +100,8 @@ def icmp_packet_unpack(data):
     icmp_type, code, checksum = struct.unpack('! B B H', data[:4])
     return icmp_type, code, checksum, data[4:]
 
-def tcp_segment(data):
+
+def tcp_segment_unpack(data):
     (source_port, destination_port, sequence, acknowledgement, offset_reserved_flags) = struct.unpack('! H H L L H', data[:14])
     offset = (offset_reserved_flags >> 12) * 4
     flag_urg = (offset_reserved_flags & 32) >> 5
@@ -69,9 +112,17 @@ def tcp_segment(data):
     flag_fin = offset_reserved_flags & 1
     return source_port, destination_port, sequence, acknowledgement, flag_urg, flag_ack, flag_psh, flag_rst, flag_syn, flag_fin, data[offset:]
 
-def udp_segment(data):
+def udp_segment_unpack(data):
     source_port, destination_port, size = struct.unpack('! H H 2x H', data[:8])
     return source_port, destination_port, size, data[8:]
+
+def format_lines(prefix, string, size=80):
+    size -= len(prefix)
+    if isinstance(string, bytes):
+        string = ''.join(r'\x{:02x}'.format(byte) for byte in string)
+        if size % 2:
+            size -= 1
+    return '/n'.join([prefix + line for line in textwrap.wrap(string,size)])
 
 
 
