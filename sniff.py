2c6d44a3196cfdb250e5b1538d7364367ed40a2c
ichthoth <shalomtata334@gmail.com>
2024-01-26 13:05:25 +0100

    unpaack tcp, udp and icmp

--- sniff.py
+++ sniff.py
@@ -51,4 +51,28 @@ def ipv4_packet_unpack(data):
 def format_ipv4(addr):
     return '.'.join(map(str,addr))
 
+# packet data protocol types
+#1 ICMP
+
+def icmp_packet_unpack(data):
+    icmp_type, code, checksum = struct.unpack('! B B H', data[:4])
+    return icmp_type, code, checksum, data[4:]
+
+def tcp_segment(data):
+    (source_port, destination_port, sequence, acknowledgement, offset_reserved_flags) = struct.unpack('! H H L L H', data[:14])
+    offset = (offset_reserved_flags >> 12) * 4
+    flag_urg = (offset_reserved_flags & 32) >> 5
+    flag_ack = (offset_reserved_flags & 16) >> 4
+    flag_psh = (offset_reserved_flags & 8)  >> 3
+    flag_rst = (offset_reserved_flags & 4)  >> 2
+    flag_syn = (offset_reserved_flags & 2)  >> 1
+    flag_fin = offset_reserved_flags & 1
+    return source_port, destination_port, sequence, acknowledgement, flag_urg, flag_ack, flag_psh, flag_rst, flag_syn, flag_fin, data[offset:]
+
+def udp_segment(data):
+    source_port, destination_port, size = struct.unpack('! H H 2x H', data[:8])
+    return source_port, destination_port, size, data[8:]
+
+
+
 main()
